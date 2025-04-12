6bfbf4a1448a18ab444a25d8629f910356f8863e
kirixxxx <119788742+kirixxxx@users.noreply.github.com>
2025-04-12 15:07:43 +0500

    Create orders.json.py

---
+++ orders.json.py
@@ -0,0 +1,56 @@
+[
+    {
+      "id": null,
+      "symbol": "ETH/USDT",
+      "side": "buy",
+      "price": 1542.65,
+      "quantity": 0.0001,
+      "status": "CANCELLED",
+      "created_at": "2025-04-12 10:01:51"
+    },
+    {
+      "id": null,
+      "symbol": "ETH/USDT",
+      "side": "buy",
+      "price": 1555.38,
+      "quantity": 0.0001,
+      "status": "CANCELLED",
+      "created_at": "2025-04-12 12:57:38"
+    },
+    {
+      "id": null,
+      "symbol": "ETH/USDT",
+      "side": "buy",
+      "price": 1568.09,
+      "quantity": 0.0001,
+      "status": "CANCELLED",
+      "created_at": "2025-04-12 13:46:37"
+    },
+    {
+        "id": null,
+        "symbol": "ETH/USDT",
+        "side": "buy",
+        "price": 1558.08,
+        "quantity": 0.0001,
+        "status": "NEW",
+        "created_at": "2025-04-12 15:15:47"
+      },
+      {
+        "id": null,
+        "symbol": "ETH/USDT",
+        "side": "buy",
+        "price": 1570.93,
+        "quantity": 0.0001,
+        "status": "NEW",
+        "created_at": "2025-04-12 15:15:51"
+      },
+      {
+        "id": null,
+        "symbol": "ETH/USDT",
+        "side": "buy",
+        "price": 1583.77,
+        "quantity": 0.0001,
+        "status": "NEW",
+        "created_at": "2025-04-12 15:15:51"
+      }
+  ]
